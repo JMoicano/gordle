4349fca89c6fcb23fabad204c66ff899d2e27a20
João Mário Soares Silva <jmariossilva@gmail.com>
2026-01-06 02:15:35 -0300

    Maven publish config

--- gordle-engine/build.gradle.kts
+++ gordle-engine/build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     kotlin("jvm") version "2.2.21"
+    `maven-publish`
+    signing
 }
 
-group = "dev.jmoicano"
-version = "1.0-SNAPSHOT"
+group = "io.github.jmoicano"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
@@ -20,4 +22,54 @@ kotlin {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            pom {
+                name.set("Gordle Engine")
+                description.set("A generic Wordle-like game engine.")
+                url.set("https://github.com/jmoicano/gordle")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("jmoicano")
+                        name.set("João Mário Soares Silva")
+                        email.set("jmariossilva@gmail.com")
+                    }
+                }
+
+                scm {
+                    url.set("https://github.com/jmoicano/gordle")
+                    connection.set("scm:git:https://github.com/jmoicano/gordle.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:jmoicano/gordle.git")
+                }
+            }
+        }
+    }
+}
+
+signing {
+    isRequired = System.getenv("CI") == "true"
+
+    useInMemoryPgpKeys(
+        System.getenv("SIGNING_KEY"),
+        System.getenv("SIGNING_PASSWORD")
+    )
+    sign(publishing.publications)
 }
